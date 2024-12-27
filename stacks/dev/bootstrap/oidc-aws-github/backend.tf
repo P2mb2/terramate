ad237def597ea2257864b0f4033059270cd75e43
Joao Cardoso <joaopedropombo@gmail.com>
2024-12-27 22:25:53 +0000

    refactor oidc

--- stacks/dev/bootstrap/oidc-aws-github/backend.tf
+++ stacks/dev/bootstrap/oidc-aws-github/backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket         = "vdfiot-template-dev-backend"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "vdfiot-template-dev-dynamodb"
     encrypt        = true
     key            = "terraform/stacks/by-id/7d22b8e6-0e02-4815-995b-1cd34c826c95/terraform.tfstate"
     region         = "eu-central-1"
